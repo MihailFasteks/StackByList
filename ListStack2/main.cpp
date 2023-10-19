d903b80a1aee7048c565cab5c2530e581397c864
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-10-19 16:25:24 +0300

    Initial Commit

---
+++ ListStack2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ListStack2
+//
+//  Created by Michalis on 19.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
